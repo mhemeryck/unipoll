fd1bfe8e0dee13fb4b23e16004925a9f792ce1d9
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2018-11-10 23:48:51 +0100

    Working poc

--- poc.py
+++ poc.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import datetime
 import os
 import sys
 
@@ -6,13 +7,13 @@ import aiofiles
 import paho.mqtt.client as mqtt
 
 CERTFILE = os.path.expanduser("~/Projects/mqtt_certs/ca.crt")
-PATH = "/sys/devices/platform/unipi_plc/io_group1/di_1_01"
-INTERVAL = 0.5
+PATH = "/sys/devices/platform/unipi_plc/io_group2/di_2_01/di_value"
+INTERVAL = 1
 
 
 class DigitalInput:
-    TRUE_VALUE = "1"
-    FALSE_VALUE = "0"
+    TRUE_VALUE = "1\n"
+    FALSE_VALUE = "0\n"
 
     def __init__(self, path, topic, callback, value=False):
         self.path = path
@@ -24,23 +25,26 @@ class DigitalInput:
         """update internal value with latest"""
         async with aiofiles.open(self.path, "r") as fh:
             updated = await fh.read() == DigitalInput.TRUE_VALUE
-            if updated != self.value:
-                self.callback(self)
+            if updated != self._value:
+                if not self._value:
+                    # Only do callback on leading edge
+                    self.callback(self)
+                self._value = updated
 
 
 async def poll(client):
     def callback(digital_input):
-        client.publish(digital_input.topic, payload="test")
+        client.publish(digital_input.topic, payload=str(datetime.now()))
 
-    digital_input = DigitalInput(PATH, "di_1_01", callback)
+    digital_input = DigitalInput(PATH, "di_2_01", callback)
     while True:
         await digital_input.update()
         asyncio.sleep(INTERVAL)
 
 
-def main(self):
+def main():
     client = mqtt.Client()
-    client.tls_set(certfile=CERTFILE)
+    client.tls_set(CERTFILE)
     client.connect("raspberrypi.lan", 8883)
 
     loop = asyncio.get_event_loop()
